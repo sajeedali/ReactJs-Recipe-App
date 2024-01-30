460aaa3f1d023e6584e3872640a46f4c337f2daf
sajeedali <saj33d739@gmail.com>
2024-01-30 20:18:51 +0530

    folder moved

--- src/components/ChefsSection.jsx
+++ src/components/ChefsSection.jsx
@@ -5,37 +5,37 @@ const ChefsSection = () => {
   const chefs = [
     {
       name: "Juan Carlos",
-      img: "/img/top-chefs/img_1.jpg",
+      img: "/public/img/top-chefs/img_1.jpg",
       recipesCount: "10",
       cuisine: "Mexican",
     },
     {
       name: "John Doe",
-      img: "/img/top-chefs/img_2.jpg",
+      img: "/public/img/top-chefs/img_2.jpg",
       recipesCount: "05",
       cuisine: "Japanese",
     },
     {
       name: "Erich Maria",
-      img: "/img/top-chefs/img_3.jpg",
+      img: "/public/img/top-chefs/img_3.jpg",
       recipesCount: "13",
       cuisine: "Italian",
     },
     {
       name: "Chris Brown",
-      img: "/img/top-chefs/img_4.jpg",
+      img: "/public/img/top-chefs/img_4.jpg",
       recipesCount: "08",
       cuisine: "American",
     },
     {
       name: "Blake Lively",
-      img: "/img/top-chefs/img_5.jpg",
+      img: "/public/img/top-chefs/img_5.jpg",
       recipesCount: "09",
       cuisine: "French",
     },
     {
       name: "Ben Affleck",
-      img: "/img/top-chefs/img_6.jpg",
+      img: "/public/img/top-chefs/img_6.jpg",
       recipesCount: "04",
       cuisine: "Indian",
     },
